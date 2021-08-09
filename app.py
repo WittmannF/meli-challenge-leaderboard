ae5ccb8f788fbb118c130517809614e9cb4d1c39
wittmannf <fernando.wittmann@gmail.com>
2021-08-09 15:04:02 -0300

    Add starter files

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Meli Kaggle-like Leaderboard')
+
+st.write('This web app is going to keep track of the Meli Leaderboard in order to always display the top scores here')
+df = pd.read_csv('./leaderboard/leaderboard_last.csv')
+cols_display = ['Username', 'Top Score', 'Submits']
+df = df[cols_display]
+df.columns = ['Username', 'Best Score', 'Submits']
+df.index = df.index+1
+st.table(df)
