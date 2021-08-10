2b2260e9eaf0449bfcb10f37ef277502923e814a
wittmannf <fernando.wittmann@gmail.com>
2021-08-10 11:45:44 -0300

    update db location

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Meli Kaggle-like Leaderboard')
 
 st.write('This web app is going to keep track of the Meli Leaderboard in order to always display the top scores here')
-df = pd.read_csv('./leaderboard/leaderboard_last.csv')
+df = pd.read_csv('./leaderboard/leaderboard.csv')
 cols_display = ['Username', 'Top Score', 'Submits']
 df = df[cols_display]
 df.columns = ['Username', 'Best Score', 'Submits']
--- etl.py
+++ etl.py
@@ -1,15 +1,13 @@
 import pandas as pd
-df_old = pd.read_csv('leaderboard/leaderboard_last.csv')
+df_old = pd.read_csv('leaderboard/leaderboard.csv')
 df_old=df_old[['Username', 'Submits', 'Top Score', 'Last Submit']]
-df_old.to_csv('leaderboard/leaderboard_old.csv', index=None)
+#df_old.to_csv('leaderboard/leaderboard_old.csv', index=None)
 df_last=pd.read_html('https://ml-challenge.mercadolibre.com/leaderboard')[0]
 df_last=df_last.join(df_old.set_index('Username'), on='Username', rsuffix='old')
-
-
 #df_last['Top Score'] = df_last.apply(lambda x: x['Score'] if (x['Score']>x['Scoreold']) else x['Scoreold'], axis=1)
 df_last['Top Score']=df_last.apply(lambda x: x['Top Score'] if x['Top Score']<x['Score'] else x['Score'], axis=1)
 df_last=df_last.sort_values('Top Score')
 
-df_last.to_csv('leaderboard/leaderboard_last.csv', index=None)
+df_last.to_csv('leaderboard/leaderboard.csv', index=None)
 
 print('Done')
