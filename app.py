bfcb603d1816900c5c16471e2078bfb2918c5eaa
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2021-08-10 17:20:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Meli Kaggle-like Leaderboard')
+DF_URL = 'https://raw.githubusercontent.com/WittmannF/meli-challenge-leaderboard/main/leaderboard/leaderboard.csv'
 
+st.title('Meli Kaggle-like Leaderboard')
 st.write('This web app is going to keep track of the Meli Leaderboard in order to always display the top scores here')
-df = pd.read_csv('./leaderboard/leaderboard.csv')
+df = pd.read_csv(DF_URL)
 cols_display = ['Username', 'Top Score', 'Submits']
 df = df[cols_display]
 df.columns = ['Username', 'Best Score', 'Submits']
