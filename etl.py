d0a736e2ef92e55c21695742135fdd3fadff9bb2
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2025-05-12 19:36:23 -0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -7,9 +7,11 @@ except:
 #df_old.to_csv('leaderboard/leaderboard_old.csv', index=None)
 df_last=pd.read_html('https://luksiensyner.github.io/', index_col=0)[0]
 df_last=df_last.join(df_old.set_index('Username'), on='Username', rsuffix='old')
-#df_last['Top Score'] = df_last.apply(lambda x: x['Score'] if (x['Score']>x['Scoreold']) else x['Scoreold'], axis=1)
-#df_last['Top Score']=df_last.apply(lambda x: x['Top Score'] if x['Top Score']<x['Score'] else x['Score'], axis=1)
-#df_last=df_last.sort_values('Top Score')
+
+score_col = 'sum of scores'
+old_score_col = score_col+'old'
+df_last['Top Score'] = df_last.apply(lambda x: x[score_col] if (x[score_col]>x[old_score_col]) else x[old_score_col], axis=1)
+df_last=df_last.sort_values('Top Score', ascending=False)
 
 df_last.to_csv('leaderboard/leaderboard_recsys.csv', index=None)
 
