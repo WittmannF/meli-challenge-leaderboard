d62f0298f82d523439c533249968ac6e2de85c5f
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2025-05-12 19:42:11 -0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -3,7 +3,10 @@ try:
   df_old = pd.read_csv('leaderboard/leaderboard_recsys.csv')
 except:
   df_old = pd.read_html('https://luksiensyner.github.io/', index_col=0)[0]
-#df_old=df_old[['Username', 'Submits', 'Top Score', 'Last Submit']]
+
+cols = [c for c in df_old.columns if 'old' not in c]
+
+df_old=df_old[cols]
 #df_old.to_csv('leaderboard/leaderboard_old.csv', index=None)
 df_last=pd.read_html('https://luksiensyner.github.io/', index_col=0)[0]
 df_last=df_last.join(df_old.set_index('Username'), on='Username', rsuffix='old')
@@ -15,4 +18,5 @@ df_last=df_last.sort_values('Top Score', ascending=False)
 
 df_last.to_csv('leaderboard/leaderboard_recsys.csv', index=None)
 
-print('Done')
+print(df_last)
+print('Done!')
